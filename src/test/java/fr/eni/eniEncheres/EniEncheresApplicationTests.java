e36c8184f9b2e9effee1f8242d9d9189dad7790f
Valentin <valent.chamboredon@gmail.com>
2025-02-10 10:02:11 +0100

    Création Projet

---
+++ src/test/java/fr/eni/eniEncheres/EniEncheresApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.eniEncheres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EniEncheresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
