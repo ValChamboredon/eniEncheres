b3b9fc29395807a7bc9fa8582c976241561e8f79
jules <lecorrejules10@gmail.com>
2025-02-10 11:29:43 +0100

    classe article

--- src/main/java/fr/eni/eniEncheres/bo/Article.java
+++ src/main/java/fr/eni/eniEncheres/bo/Article.java
@@ -2,4 +2,97 @@ package fr.eni.eniEncheres.bo;
 
 public class Article {
 
+    //attributs
+    private int noArticle;
+    private String nomArticle;
+    private String description;
+    private Date dateDebutEncheres;
+    private Date dateFinEncheres;
+    private float miseAPrix;
+    private float prixVente;
+    private int etatVente;
+
+    //constructeur
+    public Article() {
+
+    }
+
+    public Article(int noArticle, String nomArticle, String description, Date dateDebutEncheres, Date dateFinEncheres, float miseAPrix, float prixVente, int etatVente) {
+        this.noArticle = noArticle;
+        this.nomArticle = nomArticle;
+        this.description = description;
+        this.dateDebutEncheres = dateDebutEncheres;
+        this.dateFinEncheres = dateFinEncheres;
+        this.miseAPrix = miseAPrix;
+        this.prixVente = prixVente;
+        this.etatVente = etatVente;
+    }
+
+    //accesseurs et mutateurs
+    public int getNoArticle() {
+        return noArticle;
+    }
+
+    public void setNoArticle(int noArticle) {
+        this.noArticle = noArticle;
+    }
+
+    public String getNomArticle() {
+        return nomArticle;
+    }
+
+    public void setNomArticle(String nomArticle) {
+        this.nomArticle = nomArticle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDateDebutEncheres() {
+        return dateDebutEncheres;
+    }
+
+    public void setDateDebutEncheres(Date dateDebutEncheres) {
+        this.dateDebutEncheres = dateDebutEncheres;
+    }
+
+    public Date getDateFinEncheres() {
+        return dateFinEncheres;
+    }
+
+    public void setDateFinEncheres(Date dateFinEncheres) {
+        this.dateFinEncheres = dateFinEncheres;
+    }
+
+    public float getMiseAPrix() {
+        return miseAPrix;
+    }
+
+    public void setMiseAPrix(float miseAPrix) {
+        this.miseAPrix = miseAPrix;
+    }
+
+    public float getPrixVente() {
+        return prixVente;
+    }
+
+    public void setPrixVente(float prixVente) {
+        this.prixVente = prixVente;
+    }
+
+    public int getEtatVente() {
+        return etatVente;
+    }
+
+    public void setEtatVente(int etatVente) {
+        this.etatVente = etatVente;
+    }
+
+    
+
 }
