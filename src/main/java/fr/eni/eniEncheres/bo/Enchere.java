a36b417786ed031232c2a75cf0cdd3b7f702cd73
lauriane <laurianelc35@gmail.com>
2025-02-10 11:28:25 +0100

    Enchere BO

--- src/main/java/fr/eni/eniEncheres/bo/Enchere.java
+++ src/main/java/fr/eni/eniEncheres/bo/Enchere.java
@@ -1,5 +1,45 @@
 package fr.eni.eniEncheres.bo;
 
-public class Enchere {
+import java.time.LocalDateTime;
 
+public class Enchere {
+	private Integer idEnchere;
+    private LocalDateTime dateEnchere;
+    private Integer montantEnchere;
+    
+    private Utilisateur utilisateur;
+    private Article article;
+    
+    public Enchere(LocalDateTime dateEnchere, Integer montantEnchere, Utilisateur utilisateur, Article article) {
+        this.dateEnchere = dateEnchere;
+        this.montantEnchere = montantEnchere;
+        this.utilisateur = utilisateur;
+        this.article = article;
+    }
+
+	public Integer getIdEnchere() {
+		return idEnchere;
+	}
+
+	public void setIdEnchere(Integer idEnchere) {
+		this.idEnchere = idEnchere;
+	}
+
+	public LocalDateTime getDateEnchere() {
+		return dateEnchere;
+	}
+
+	public void setDateEnchere(LocalDateTime dateEnchere) {
+		this.dateEnchere = dateEnchere;
+	}
+
+	public Integer getMontantEnchere() {
+		return montantEnchere;
+	}
+
+	public void setMontantEnchere(Integer montantEnchere) {
+		this.montantEnchere = montantEnchere;
+	}
+
+    
 }
