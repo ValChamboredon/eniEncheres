f4e3dabdc08490e345bdfd78b73b00d19e72fb44
lauriane <laurianelc35@gmail.com>
2025-02-12 16:20:18 +0100

    Visuel OK

--- src/main/java/fr/eni/eniEncheres/bo/EtatVente.java
+++ src/main/java/fr/eni/eniEncheres/bo/EtatVente.java
@@ -1,8 +1,18 @@
 package fr.eni.eniEncheres.bo;
 
 public enum EtatVente {
-	CREEE,
-    EN_COURS,
-    ENCHERES_TERMINEES,
-    RETRAIT_EFFECTUE
+    CREEE("CREEE"),
+    EN_COURS("EN_COURS"),
+    ENCHERES_TERMINEES("ENCHERES_TERMINEES"),
+    RETRAIT_EFFECTUE("RETRAIT_EFFECTUE");
+
+    private final String valeur;
+
+    EtatVente(String valeur) {
+        this.valeur = valeur;
+    }
+
+    public String getValeur() {
+        return valeur;
+    }
 }
