b7c284133a8017a5152fa44430e0b097231bd661
lauriane <laurianelc35@gmail.com>
2025-02-12 16:46:37 +0100

    Visuels corrects, fonctionnalités à vérifier

--- src/main/java/fr/eni/eniEncheres/controller/EnchereController.java
+++ src/main/java/fr/eni/eniEncheres/controller/EnchereController.java
@@ -1,9 +1,11 @@
 package fr.eni.eniEncheres.controller;
 
 
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,7 +13,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.eni.eniEncheres.bll.ArticleService;
+import fr.eni.eniEncheres.bll.CategorieService;
 import fr.eni.eniEncheres.bll.EnchereService;
+import fr.eni.eniEncheres.bo.ArticleVendu;
 import fr.eni.eniEncheres.bo.Enchere;
 import fr.eni.eniEncheres.exception.BusinessException;
 
@@ -20,6 +25,12 @@ import fr.eni.eniEncheres.exception.BusinessException;
 public class EnchereController {
 
     private final EnchereService enchereService;
+    
+    @Autowired
+    private ArticleService articleService;
+    
+    @Autowired
+    private CategorieService categorieService;
 
     @Autowired
     public EnchereController(EnchereService enchereService) {
@@ -39,5 +50,25 @@ public class EnchereController {
     @GetMapping("/max/{id}")
     public Enchere getMeilleureEnchere(@PathVariable int id) throws BusinessException {
         return enchereService.getEnchereMaxParArticle(id);
+    }
+    
+    @GetMapping
+    public String listeEncheres(Model model) {
+        try {
+            // Ajoutez des logs pour debugger
+            System.out.println("Chargement des articles");
+            List<ArticleVendu> articles = articleService.getArticlesEnVente();
+            System.out.println("Nombre d'articles : " + articles.size());
+            
+            model.addAttribute("articles", articles);
+            model.addAttribute("categories", categorieService.getAllCategories());
+            
+            return "listeArticles"; 
+        } catch (Exception e) {
+            // Log de l'erreur complète
+            e.printStackTrace();
+            model.addAttribute("erreurs", Collections.singletonList("Erreur de chargement : " + e.getMessage()));
+            return "erreur";
+        }
     }
 }
