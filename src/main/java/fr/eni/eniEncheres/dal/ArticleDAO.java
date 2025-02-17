14a415334b7aebdf0ced45b37148002e284ef01b
Valentin <valent.chamboredon@gmail.com>
2025-02-17 16:03:05 +0100

    filtre Ventes

--- src/main/java/fr/eni/eniEncheres/dal/ArticleDAO.java
+++ src/main/java/fr/eni/eniEncheres/dal/ArticleDAO.java
@@ -6,23 +6,29 @@ import fr.eni.eniEncheres.bo.ArticleVendu;
 import fr.eni.eniEncheres.bo.EtatVente;
 
 public interface ArticleDAO {
-	
+
 	ArticleVendu getArticleById(int noArticle);
-    List<ArticleVendu> getAllArticles(); // itération 1 
-    void addArticle(ArticleVendu article); // itération 1
-    
-    List<ArticleVendu> getArticlesByCategory(int categoryId);
-    List<ArticleVendu> getArticlesByUser(int userId);
-    List<ArticleVendu> getArticlesEnVente();
-    List<ArticleVendu> searchArticles(String keyword, int categoryId);
-    List<ArticleVendu> getArticlesTermines();
-    
-    void updateEtatVente(int noArticle, EtatVente nouvelEtat);
-    void deleteArticle(int articleId);
 
-    public void modifierArticle(ArticleVendu article);
+	List<ArticleVendu> getAllArticles(); // itération 1
 
- 
+	void addArticle(ArticleVendu article); // itération 1
 
-    
+	List<ArticleVendu> getArticlesByCategory(int categoryId);
+
+	List<ArticleVendu> getArticlesByUser(int userId);
+
+	List<ArticleVendu> getArticlesEnVente();
+
+	List<ArticleVendu> searchArticles(String keyword, int categoryId);
+
+	List<ArticleVendu> getArticlesTermines();
+
+	void updateEtatVente(int noArticle, EtatVente nouvelEtat);
+
+	void deleteArticle(int articleId);
+
+	public void modifierArticle(ArticleVendu article);
+
+	List<ArticleVendu> filtrerVentes(int userId, Boolean ventesEnCours, Boolean ventesNonDebutees,Boolean ventesTerminees);
+
 }
