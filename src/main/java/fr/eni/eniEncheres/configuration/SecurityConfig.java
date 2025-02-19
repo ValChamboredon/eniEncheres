ded386ff1cff21994204198bb5e8fe7509aac85d
jules <lecorrejules10@gmail.com>
2025-02-19 14:07:09 +0100

    suppression compte quand admin + remember login

--- src/main/java/fr/eni/eniEncheres/configuration/SecurityConfig.java
+++ src/main/java/fr/eni/eniEncheres/configuration/SecurityConfig.java
@@ -2,19 +2,30 @@ package fr.eni.eniEncheres.configuration;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.rememberme.InMemoryTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import fr.eni.eniEncheres.bll.UtilisateurService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	
+	@Autowired
+	private RememberMe rememberMe;
 
 	@Bean
 	SecurityFilterChain getFilterChain(HttpSecurity security) throws Exception {
@@ -31,9 +42,14 @@ public class SecurityConfig {
 	        // Utilisation de anyRequest() en dernier
 	        auth.anyRequest().authenticated();
 	    });
-
+	    
 	    security.formLogin(formLogin -> {
-	        formLogin.loginPage("/connexion").defaultSuccessUrl("/encheres", true).permitAll();
+	        formLogin
+	        	.loginPage("/connexion")
+	        	.loginProcessingUrl("/connexion")
+	        	.successHandler(rememberMe)
+//	        	.defaultSuccessUrl("/encheres", true)
+	        	.permitAll();
 	    });
 
 	    security.logout(logout -> logout.invalidateHttpSession(true)
@@ -57,4 +73,5 @@ public class SecurityConfig {
 
 		return userManager;
 	}
+
 }
