858a04d8427c8e8d7324a4e5bc87b17127429e53
jules <lecorrejules10@gmail.com>
2025-02-19 14:41:04 +0100

    suppression compte quand admin + remember login

--- src/main/java/fr/eni/eniEncheres/controller/ArticleController.java
+++ src/main/java/fr/eni/eniEncheres/controller/ArticleController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
->>>>>>> jules
 import org.springframework.web.bind.annotation.*;
 
 import fr.eni.eniEncheres.bll.ArticleService;
@@ -29,6 +28,7 @@ import fr.eni.eniEncheres.bll.CategorieService;
 import fr.eni.eniEncheres.bll.UtilisateurService;
 import fr.eni.eniEncheres.bo.ArticleVendu;
 import fr.eni.eniEncheres.bo.Categorie;
+import fr.eni.eniEncheres.bo.EtatVente;
 import fr.eni.eniEncheres.bo.Retrait;
 import fr.eni.eniEncheres.bo.Utilisateur;
 import fr.eni.eniEncheres.exception.BusinessException;
@@ -172,18 +172,6 @@ public class ArticleController {
     }
 
     /**
-     * Supprime un article.
-     */
-    @PostMapping("/supprimer")
-    public String supprimerArticle(@RequestParam("articleId") int articleId, Principal principal) {
-        ArticleVendu article = articleService.getArticleById(articleId);
-        if (article != null && article.getVendeur().getEmail().equals(principal.getName())) {
-            articleService.supprimerArticle(articleId);
-        }
-        return "redirect:/encheres";
-    }
-
-    /**
      * Modifie un article.
      */
     @PostMapping("/modifier")
@@ -207,119 +195,8 @@ public class ArticleController {
         return "redirect:/encheres/article/detail/" + articleId;
     }
     
-<<<<<<< HEAD
-    @ModelAttribute("creditsUtilisateur")
-=======
-	@GetMapping("/vendeur")
-	public String afficherProfilVendeur(@RequestParam("pseudo") String pseudo, Model model, HttpSession session) {
-		//retrouver le vendeur de l'article
-		Utilisateur vendeur = utilisateurService.getUtilisateurByPseudo(pseudo);
-		model.addAttribute("vendeur", vendeur);
-		
-		//récupérer l'utilisateur actif
-		Authentication authentification = SecurityContextHolder.getContext().getAuthentication();
-		String email = authentification.getName();
-		System.out.println(email);
-		int userId = utilisateurService.getIdByEmail(email);
-		Utilisateur utilisateur = utilisateurService.getUtilisateurById(userId);
+    //@ModelAttribute("creditsUtilisateur")
 
-	    if (utilisateur != null) {
-	        model.addAttribute("isAdmin", utilisateur.isAdministrateur());  // Assurez-vous que isAdmin est ajouté
-	        model.addAttribute("vendeur", utilisateur);  // Si tu utilises vendeur pour l'utilisateur
-	    } else {
-	        model.addAttribute("isAdmin", false);  // Si l'utilisateur n'est pas connecté ou admin
-	    }
-		
-		return "vendeur-profil";
-	}
-
-
-	/**
-	 * Charge les catégories en session pour toutes les pages.
-	 */
-	@ModelAttribute("CategoriesEnSession")
-	public List<Categorie> chargerCategories() throws BusinessException {
-		return categorieService.getAllCategories();
-	}
-
-	/**
-	 * Affiche le formulaire de mise en vente d'un article.
-	 */
-	@GetMapping("/vendre")
-	public String afficherFormulaireVendreArticle(Model model, Principal principal) {
-		model.addAttribute("article", new ArticleVendu());
-
-		if (principal != null) {
-			Utilisateur utilisateur = utilisateurService.getUtilisateurByEmail(principal.getName());
-			model.addAttribute("utilisateurConnecte", utilisateur); // Envoie les infos de l'utilisateur à Thymeleaf
-		}
-
-		return "formulaireArticle";
-	}
-
-	/**
-	 * Enregistre un article mis en vente.
-	 */
-	@PostMapping("/vendre")
-	public String mettreArticleEnVente(@Valid @ModelAttribute("article") ArticleVendu article,
-			BindingResult bindingResult, Model model, Principal principal, @RequestParam("image") MultipartFile image) {
-		if (bindingResult.hasErrors()) {
-			return "formulaireArticle";
-		}
-        
-        // Définir le dossier de destination
-//        File destinationDir = new File("");
-//        if (!destinationDir.exists()) {
-//        	destinationDir.mkdirs(); // Crée le dossier s'il n'existe pas
-//        }
-//
-//        // Construire le chemin du fichier
-//        File destinationFile = new File(uploadDir + image.getOriginalFilename());
-//
-//        // Sauvegarde du fichier
-//        try {
-//			image.transferTo(destinationFile);
-//		} catch (IllegalStateException | IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-        
-
-		if (principal != null) {
-			Utilisateur utilisateur = utilisateurService.getUtilisateurByEmail(principal.getName());
-			article.setVendeur(utilisateur);
-
-			try {
-				articleService.creerArticle(article);
-				return "redirect:/encheres";
-			} catch (BusinessException be) {
-				bindingResult.addError(new ObjectError("globalError", be.getMessage()));
-				return "formulaireArticle";
-			}
-		}
-
-		return "redirect:/encheres/connexion";
-	}
-
-	/**
-	 * Affiche le détail d'un article.
-	 */
-	@GetMapping("/article/detail/{noArticle}")
-	public String afficherDetailArticle(@PathVariable("noArticle") int noArticle, Model model, Principal principal) {
-		ArticleVendu article = articleService.getArticleById(noArticle);
-
-		if (article == null) {
-			return "redirect:/encheres";
-		}
-
-		if (article.getLieuDeRetrait() == null) {
-			article.setLieuDeRetrait(new Retrait(article.getVendeur().getRue(), article.getVendeur().getCodePostal(),
-					article.getVendeur().getVille()));
-		}
-
-		model.addAttribute("article", article);
-		return "detailArticle";
-	}
 
 	/**
 	 * Supprime un article si l'utilisateur connecté est bien le vendeur.
@@ -358,38 +235,9 @@ public class ArticleController {
 	}
 
 	/**
-	 * Modifie un article si l’utilisateur est le vendeur et que l’état est encore
-	 * `CREEE`.
-	 */
-	@PostMapping("/modifier")
-	public String modifierArticle(@RequestParam("articleId") int articleId,
-			@RequestParam("nomArticle") String nomArticle, @RequestParam("description") String description,
-			@RequestParam("miseAPrix") int miseAPrix,
-			@RequestParam("dateDebutEncheres") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateDebutEncheres,
-			@RequestParam("dateFinEncheres") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateFinEncheres,
-			Principal principal) {
-
-		ArticleVendu article = articleService.getArticleById(articleId);
-
-		if (article != null && article.getEtatVente() == EtatVente.CREEE
-				&& article.getVendeur().getEmail().equals(principal.getName())) {
-			article.setNomArticle(nomArticle);
-			article.setDescription(description);
-			article.setMiseAPrix(miseAPrix);
-			article.setDateDebutEncheres(dateDebutEncheres);
-			article.setDateFinEncheres(dateFinEncheres);
-
-			articleService.modifierArticle(article);
-		}
-
-		return "redirect:/encheres/article/detail/" + articleId;
-	}
-
-	/**
 	 * Charge les crédits de l'utilisateur connecté.
 	 */
 	@ModelAttribute("creditsUtilisateur")
->>>>>>> jules
 	public Integer chargerCreditsUtilisateur(Principal principal) {
 		if (principal != null) {
 			Utilisateur utilisateur = utilisateurService.getUtilisateurByEmail(principal.getName());
@@ -434,17 +282,5 @@ public class ArticleController {
 		
 		return "vendeur-profil";
 	}
-    
-    @GetMapping("/modifier")
-    public String afficherFormulaireModification(@RequestParam("articleId") int articleId, Model model, Principal principal) {
-        ArticleVendu article = articleService.getArticleById(articleId);
-
-        if (article != null && article.getVendeur().getEmail().equals(principal.getName())) {
-            model.addAttribute("article", article);
-            return "modifierArticle"; // Assurez-vous que cette vue existe !
-        }
-
-        return "redirect:/encheres"; // Redirection si l'article n'existe pas ou que l'utilisateur n'a pas les droits
-    }
 
 }
